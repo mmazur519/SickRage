f07a9e4661eced13b204f2ac486f8d32e9969213
Michael Mazur <mmazurlaptop@gmail.com>
2014-06-21 10:10:43 -0400

    Change To Pushbullet
    Small changes to pushbullet notification

--- gui/slick/js/configNotifications.js
+++ gui/slick/js/configNotifications.js
@@ -236,12 +236,13 @@ $(document).ready(function(){
     $('#testPushbullet').click(function () {
         $('#testPushbullet-result').html(loading);
         var pushbullet_api = $("#pushbullet_api").val();
-        if($("#pushbullet_api").val() == '') {
+        var pushbullet_device = $("#pushbullet_device_list").val();
+		if($("#pushbullet_api").val() == '') {
             $('#testPushbullet-result').html("You didn't supply a Pushbullet api key");
             $("#pushbullet_api").focus();
             return false;
         }
-        $.get(sbRoot + "/home/testPushbullet", {'api': pushbullet_api},
+        $.get(sbRoot + "/home/testPushbullet", {'api': pushbullet_api, 'device': pushbullet_device},
             function (data) {
                 $('#testPushbullet-result').html(data);
             }
@@ -255,6 +256,7 @@ $(document).ready(function(){
         }
         
         var pushbullet_api = $("#pushbullet_api").val();
+		var pushbullet_devices = $("#pushbullet_device_list").val();
 
         if(!pushbullet_api) {
             $('#testPushbullet-result').html("You didn't supply a Pushbullet api key");
--- sickbeard/notifiers/pushbullet.py
+++ sickbeard/notifiers/pushbullet.py
@@ -27,7 +27,7 @@ from sickbeard import logger, common
 
 
 class PushbulletNotifier:
-    def test_notify(self, pushbullet_api):
+    def test_notify(self, pushbullet_api, pushbullet_device):
         return self._sendPushbullet(pushbullet_api, event="Test", message="Testing Pushbullet settings from SickRage",
                                     method="POST", notificationType="note", force=True)
 
